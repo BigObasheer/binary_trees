1638222de893fff0fb87cc94b4308ca1e8986013
BigOBasheer <838@holbertonschool.com>
2020-11-25 05:46:16 +0000

    Add Task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,9 +9,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
-	if (node->parent->parent->left == node->parent)
-		return (node->parent->parent->right);
+	if (node->parent->left == node)
+		return (node->parent->right);
 	else
-		return (node->parent->parent->left);
-
+		return (node->parent->left);
 }
